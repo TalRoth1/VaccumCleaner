ff4ebf43808469f39de0ad2733f6f3d2ba13ff27
Tal Roth <rothta@post.bgu.ac.il>
2024-12-24 18:30:01 +0200

    ready for testing

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -1,12 +1,16 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.MessageBusImpl;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.TickBroadcast;
 
 /**
  * TimeService acts as the global timer for the system, broadcasting TickBroadcast messages
  * at regular intervals and controlling the simulation's duration.
  */
 public class TimeService extends MicroService {
+    private int tickTime;
+    private int duration;
 
     /**
      * Constructor for TimeService.
@@ -14,9 +18,11 @@ public class TimeService extends MicroService {
      * @param TickTime  The duration of each tick in milliseconds.
      * @param Duration  The total number of ticks before the service terminates.
      */
-    public TimeService(int TickTime, int Duration) {
-        super("Change_This_Name");
-        // TODO Implement this
+    public TimeService(int TickTime, int Duration)
+    {
+        super("TimeService");
+        this.tickTime = tickTime;
+        this.duration = duration;
     }
 
     /**
@@ -24,7 +30,20 @@ public class TimeService extends MicroService {
      * Starts broadcasting TickBroadcast messages and terminates after the specified duration.
      */
     @Override
-    protected void initialize() {
-        // TODO Implement this
+    protected void initialize() 
+    {
+        for(int tick = 1; tick< duration; tick++)
+        {
+            try
+            {
+                Thread.sleep(tickTime);
+                sendBroadcast(new TickBroadcast(tick));
+            } 
+            catch (Exception e) 
+            {
+                super.terminate();
+            }
+        }
+        terminate();
     }
 }
